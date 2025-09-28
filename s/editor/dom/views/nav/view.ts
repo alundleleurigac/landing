6f7c95a0b077e011b4fcf1b33e1f97d127f7bace
Chase Moskal <chasemoskal@gmail.com>
2025-09-28 02:14:28 -0700

    tweak: nav renderLink

--- s/editor/dom/views/nav/view.ts
+++ s/editor/dom/views/nav/view.ts
@@ -1,5 +1,5 @@
 import {html} from "lit"
-import {view} from "@e280/sly"
+import {spa, view} from "@e280/sly"
 
 import styleCss from "./style.css.js"
 import themeCss from "../../../theme.css.js"
@@ -9,12 +9,18 @@ export const Nav = view(use => (context: EditorContext) => {
 	const {router} = context
 	use.styles(themeCss, styleCss)
 
+	function renderLink(label: string, navigable: spa.Navigable) {
+		const hash = navigable.hash()
+		const className = navigable.active ? "active" : ""
+		return html`<a href="${hash}" class="${className}">${label}</a>`
+	}
+
 	return html`
 		<h2>Omniclip</h2>
 		<div class="links">
-			<a href="${router.nav.home.hash()}" class="${router.nav.home.active ? "active" : ""}">About</a>
-			<a href="${router.nav.projects.hash()}" class="${router.nav.projects.active ? "active" : ""}">Projects</a>
-			<a href="${router.nav.account.hash()}" class="${router.nav.account.active ? "active" : ""}">Account</a>
+			${renderLink("About", router.nav.home)}
+			${renderLink("Projects", router.nav.projects)}
+			${renderLink("Account", router.nav.account)}
 		</div>
 	`
 })
