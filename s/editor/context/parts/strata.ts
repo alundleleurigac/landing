1ac3f77859f9a82b3514fd5af3cb33c663f951b2
Przemek <przemekgg2002@gmail.com>
2025-10-02 17:50:19 +0200

    add: basic project settings to context state

--- s/editor/context/parts/strata.ts
+++ s/editor/context/parts/strata.ts
@@ -8,6 +8,14 @@ export type State = {
 	chron: Chronicle<{
 		timeline: {}
 	}>
+	settings: {
+		timebase: number
+		zoom: number
+		resolution: {
+			width: number
+			height: number
+		}
+	}
 }
 
 export class Strata {
@@ -18,8 +26,17 @@ export class Strata {
 		chron: Trunk.chronicle({
 			timeline: {},
 		}),
+		settings: {
+			timebase: 30,
+			zoom: 1,
+			resolution: {
+				width: 1920,
+				height: 1080
+			}
+		}
 	})
 
+	settings = this.trunk.branch(s => s.settings)
 	files = this.trunk.branch(s => s.files)
 	timeline = this.trunk.chronobranch(64, s => s.chron)
 }
