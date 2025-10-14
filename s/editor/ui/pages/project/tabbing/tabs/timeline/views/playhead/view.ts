e46a93c05e6d1503322d75ec5be3dc2452816dcc
Przemek <przemekgg2002@gmail.com>
2025-10-14 15:59:50 +0200

    add: playhead movement based on player currentTime

--- s/editor/ui/pages/project/tabbing/tabs/timeline/views/playhead/view.ts
+++ s/editor/ui/pages/project/tabbing/tabs/timeline/views/playhead/view.ts
@@ -2,13 +2,19 @@ import {html} from "lit"
 import {view} from "@e280/sly"
 
 import styleCss from "./style.css.js"
+import {PIXELS_PER_MILLISECOND} from "../../constants.js"
+import {EditorContext} from "../../../../../../../../context/context.js"
 import playheadSvg from "../../../../../../../icons/remix-icon/playhead.svg.js"
 
-export const Playhead = view(use => () => {
+export const Playhead = view(use => (context: EditorContext) => {
 	use.styles(styleCss)
 
+	const player = context.controllers.player
+	const zoom = context.strata.settings.state.zoom
+	const position = (player.currentTime.value * 1000) * PIXELS_PER_MILLISECOND * zoom
+
 	return html`
-		<div class="playhead" style="left: ${0}px">
+		<div class="playhead" style="left: ${position}px">
 			${playheadSvg}
 			<div class="line"></div>
 		</div>
